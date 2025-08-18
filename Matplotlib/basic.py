9c583712bcb83fa85f10acf7b9d09ea5329aa7de
pegalearn1 <pegalearn1@gmail.com>
2025-08-18 18:32:54 +0530

    updated basics too scatter

--- Matplotlib/basic.py
+++ Matplotlib/basic.py
@@ -1,18 +1,26 @@
-import matplotlib
+'''
+I have created this file to learn the basics of matplotlib library of python. 
+Where i have included keypoints in terms of different functions. 
+You can call the particular function to see the particular output.
 
+'''
+
+#Common imports
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+#to check the version of matplotlib
 def version():
     """Prints the version of matplotlib."""
     print("Matplotlib version:")
     print( matplotlib.__version__)
 
-#common imports
-import matplotlib.pyplot as plt
-import numpy as np
 
 
 #to draw a line in a plot
 #x and y points
-
 def st_line():
     print("This is a line function")
     xpoints = np.array([0, 6])
@@ -60,6 +68,8 @@ def st_default_x2():
     plt.plot(ypoints, marker = 'o')
     plt.show()
 
+
+
 #to display markers with the line and marker as a *
 def st_default_x3():
     print("This is a default x points function with more points")
@@ -67,6 +77,7 @@ def st_default_x3():
 
     plt.plot(ypoints, marker = '*')
     plt.show()
+
 
 #to give a shortcut for the marker we have fmt method
 def fm_marker():
@@ -183,3 +194,236 @@ def grid():
     plt.grid(color = 'red', ls = '-.', lw = 1) #color, linestyle, linewidth, alpha
     
     plt.show()
+
+
+#to add subplots in a single plot
+#where in plt.subplot(1, 4, 1) 1st is number of rows, 2nd is columns and 3rd is on what position the plot will be.
+def subplots():
+    ypoints = np.array([2, 6, 7, 1, 4, 3])
+    plt.subplot(1, 4, 1)
+    plt.plot(ypoints, color = 'red',  lw = 2 )
+
+    spoints = np.array([1, 8, 4, 2, 5, 4])
+    plt.subplot(1, 4, 2)
+    plt.plot(spoints, color = 'green', lw = 2 )
+
+    dpoints = np.array([1, 8, 4, 2, 5, 4])
+    plt.subplot(1, 4, 3)
+    plt.plot(dpoints, color = 'blue', lw = 2 )
+
+    tpoints = np.array([1, 8, 4, 2, 5, 4])
+    plt.subplot(1, 4, 4)
+    plt.plot(tpoints, color = 'yellow', lw = 2 )
+
+    plt.show()
+
+
+#to show the scatter plot
+#where x and y are the points to be plotted
+'''
+The observation in the example above is the result of 13 cars passing by.
+
+The X-axis shows how old the car is.
+
+The Y-axis shows the speed of the car when it passes.
+
+'''
+def scatter():
+    print("This is a scatter plot function")
+    #example of scatter plot
+    x = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
+    y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
+    plt.scatter(x,y)
+    
+    plt.title("Diffrence b/w car's age and speed", loc='center')
+    plt.xlabel("Age of Cars", loc='center')
+    plt.ylabel("Speed of Cars", loc='center')
+    
+    plt.show()
+
+
+'''
+In the example above, there seems to be a relationship between speed and age, 
+but what if we plot the observations from another day as well? 
+Will the scatter plot tell us something else?
+
+'''
+def scatter2():
+    #day one, the age and speed of 13 cars:
+    x = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
+    y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
+    plt.scatter(x, y, marker = 'o', color = 'blue', label = 'Day 1')
+
+    #day two, the age and speed of 15 cars:
+    x = np.array([2,2,8,1,15,8,12,9,7,3,11,4,7,14,12])
+    y = np.array([100,105,84,105,90,99,90,95,94,100,79,112,91,80,85])
+    plt.scatter(x, y, marker = 'o', color = 'red', label = 'Day 2')
+
+    plt.title("Diffrence b/w car's age and speed", loc='center')
+    plt.xlabel("Age of Cars", loc='center')
+    plt.ylabel("Speed of Cars", loc='center')
+
+    plt.show()
+
+
+#displaying each dot with different colors
+def multi_color_scatter():
+    print("This is a scatter plot function")
+    #example of scatter plot
+    x = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
+    y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
+    colors = np.array(["red","green","blue","yellow","pink","black","orange","purple","beige","brown","gray","cyan","magenta"])
+    
+    plt.scatter(x,y, c=colors)
+    
+    plt.title("Diffrence b/w car's age and speed", loc='center')
+    plt.xlabel("Age of Cars", loc='center')
+    plt.ylabel("Speed of Cars", loc='center')
+    
+    plt.show()
+
+
+'''
+The Matplotlib module has a number of available colormaps.
+
+A colormap is like a list of colors, where each color has a value that ranges from 0 to 100.
+
+'''
+def colormap():
+
+    #day one, the age and speed of 13 cars:
+    x = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
+    y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
+    colors1 = np.array([0, 10, 20, 30, 40, 45, 50, 55, 60, 70, 80, 90, 100])
+    plt.scatter(x, y, marker = 'o', c = colors1, cmap='viridis', label = 'Day 1') # 'viridis' is a colormap, you can change it to other colormaps like 'plasma', 'inferno', 'magma', etc.
+
+    #day two, the age and speed of 15 cars:
+    x = np.array([2,2,8,1,15,8,12,9,7,3,11,4,7])
+    y = np.array([100,105,84,105,90,99,90,95,94,100,79,112,91])
+    colors2 = np.array([0, 10, 20, 30, 40, 45, 50, 55, 60, 70, 80, 90, 100])
+    plt.scatter(x, y, marker = 'o', c = colors2, cmap='inferno', label = 'Day 2') # 'inferno' is a colormap, you can change it to other colormaps like 'viridis', 'plasma', 'inferno', 'magma', etc.
+
+    plt.title("Diffrence b/w car's age and speed", loc='center')
+    plt.xlabel("Age of Cars", loc='center')
+    plt.ylabel("Speed of Cars", loc='center')
+
+    plt.colorbar() #to add and show the color bar with the drawing
+    plt.legend() #to show the legend of the plot - means the label of the plot
+    
+    plt.show()
+
+'''
+colomaps in matplotlib:
+
+Name		 	Reverse	
+
+Accent		 	Accent_r	
+Blues		 	Blues_r	
+BrBG		 	BrBG_r	
+BuGn		 	BuGn_r	
+BuPu		 	BuPu_r	
+CMRmap		 	CMRmap_r	
+Dark2		 	Dark2_r	
+GnBu		 	GnBu_r	
+Greens		 	Greens_r	
+Greys		 	Greys_r	
+OrRd		 	OrRd_r	
+Oranges		 	Oranges_r	
+PRGn		 	PRGn_r	
+Paired		 	Paired_r	
+Pastel1		 	Pastel1_r	
+Pastel2		 	Pastel2_r	
+PiYG		 	PiYG_r	
+PuBu		 	PuBu_r	
+PuBuGn		 	PuBuGn_r	
+PuOr		 	PuOr_r	
+PuRd		 	PuRd_r	
+Purples		 	Purples_r	
+RdBu		 	RdBu_r	
+RdGy		 	RdGy_r	
+RdPu		 	RdPu_r	
+RdYlBu		 	RdYlBu_r	
+RdYlGn		 	RdYlGn_r	
+Reds		 	Reds_r	
+Set1		 	Set1_r	
+Set2		 	Set2_r	
+Set3		 	Set3_r	
+Spectral		Spectral_r	
+Wistia		 	Wistia_r	
+YlGn		 	YlGn_r	
+YlGnBu		 	YlGnBu_r	
+YlOrBr		 	YlOrBr_r	
+YlOrRd		 	YlOrRd_r	
+afmhot		 	afmhot_r	
+autumn		 	autumn_r	
+binary		 	binary_r	
+bone		 	bone_r	
+brg		 	    brg_r	
+bwr		 	    bwr_r	
+cividis		 	cividis_r	
+cool		 	cool_r	
+coolwarm		coolwarm_r	
+copper		 	copper_r	
+cubehelix		cubehelix_r	
+flag		 	flag_r	
+gist_earth		gist_earth_r	
+gist_gray		gist_gray_r	
+gist_heat		gist_heat_r	
+gist_ncar		gist_ncar_r	
+gist_rainbow	gist_rainbow_r	
+gist_stern		gist_stern_r	
+gist_yarg		gist_yarg_r	
+gnuplot		 	gnuplot_r	
+gnuplot2		gnuplot2_r	
+gray		 	gray_r	
+hot		 	    hot_r	
+hsv		 	    hsv_r	
+inferno		 	inferno_r	
+jet		 	    jet_r	
+magma		 	magma_r	
+nipy_spectral	nipy_spectral_r	
+ocean		 	ocean_r	
+pink		 	pink_r	
+plasma		 	plasma_r	
+prism		 	prism_r	
+rainbow		 	rainbow_r	
+seismic		 	seismic_r	
+spring		 	spring_r	
+summer		 	summer_r	
+tab10		 	tab10_r	
+tab20		 	tab20_r	
+tab20b		 	tab20b_r	
+tab20c		 	tab20c_r	
+terrain		 	terrain_r	
+twilight		twilight_r	
+twilight_shifted	twilight_shifted_r	
+viridis		 	viridis_r	
+winter		 	winter_r
+
+
+'''
+
+#tells you how to add size and alpha(tranparency) of the scatter points
+def size_and_alpha():
+    print("This is a scatter plot function")
+    #example of scatter plot
+    x = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
+    y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
+    colors = np.array([0, 10, 20, 30, 40, 45, 50, 55, 60, 70, 80, 90, 100])
+    sizes = np.array([20,50,100,200,500,1000,60,90,10,300,600,800,75])
+    
+    plt.scatter(x,y, c=colors, cmap = 'plasma', s = sizes, alpha = 0.5, label = 'Test') #s is the size of the marker, alpha is the transparency of the marker
+    
+    plt.title("Diffrence b/w car's age and speed", loc='center')
+    plt.xlabel("Age of Cars", loc='center')
+    plt.ylabel("Speed of Cars", loc='center')
+
+    plt.colorbar() #to add and show the color bar with the drawing
+    plt.legend() #to show the legend of the plot - means the label of the plot
+
+    plt.show()
+
+
+
+
+
