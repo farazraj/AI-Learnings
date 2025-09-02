e26838ae11108f1ae802b96a9fd60e7e6f674b5d
pegalearn1 <pegalearn1@gmail.com>
2025-09-02 17:42:32 +0530

    pandas update

--- Pandas/basics_pandas.py
+++ Pandas/basics_pandas.py
@@ -92,8 +92,93 @@ def load_files():
     #csv
     df = pd.read_csv('data.csv')
 
+    #json
+    dfj = pd.read_json('data.json')
+
+    
     print("csv file - ")
-    print(df, "\n")
+    print(df, "\n") #prints the whole dataframe
 
-load_files()
+    print("json file - ")
+    print(dfj, "\n") #prints the whole dataframe
 
+    print(pd.options.display.max_rows) #prints the max rows that can be displayed
+    
+
+#data cleaning
+def data_cleaning():
+    df = pd.read_csv('data_clean.csv')
+
+    print(df.to_string(), "\n") #prints the whole dataframe
+
+    dfc = df.dropna() #drops the rows with null values
+    print("after dropping null values - ")
+    print(dfc.to_string(), "\n")
+
+
+def data_replacement():
+    df = pd.read_csv('data_clean.csv')
+
+    print(df.to_string(), "\n") #prints the whole dataframe
+
+    dfr = df.fillna(130) #replaces the null values with 130
+    print("after replacing null values - ")
+    print(dfr.to_string(), "\n")
+
+    dfc = df.fillna({'Calories':130, 'Duration':45}) #replaces the null values in specific columns
+    print("after replacing null values to column - ")
+    print(dfc.to_string(), "\n")
+
+
+
+#dropping a column with null values using name 
+def data_drop_column():
+    df = pd.read_csv('data_clean.csv')
+
+
+    df.dropna(subset = ['Calories'], inplace=True) #drops the rows with null values in specific column
+
+    '''when using inplace=True, the original dataframe is modified and nothing is returned so,
+    reassigning it to another variable like df = df.dropna(subset = ['Calories'], inplace=True)  
+    will return NoneType error'''
+
+    print(df.to_string())
+
+
+#changing the format of data
+def change_format():
+
+    df = pd.read_csv('data_w_date.csv')
+
+    print(df.to_string(), "\n") #prints the whole dataframe
+
+    # df['Date'] = df['Date'].str.strip("'")
+    df['Date'] = pd.to_datetime(df['Date'], format='mixed') #changes the format of date column to datetime
+    print("after changing the format of date column - ")
+    print(df.to_string(), "\n")
+
+
+
+
+
+#to find out if it row has duplicate rows
+def duplicate_rows():
+    df = pd.read_csv('data_w_date.csv')
+
+    print(df.duplicated())
+
+    df.drop_duplicates(inplace=True) #drops the duplicate rows
+    print(df.to_string())
+
+
+#relationship between two columns
+def relation_columns():
+    df = pd.read_csv('data_clean.csv')
+
+    print(df.to_string(), "\n") #prints the whole dataframe
+
+    print("relation between two columns - ")
+    print(df.corr().to_string(), "\n") #prints the relation between two columns
+
+
+relation_columns()
