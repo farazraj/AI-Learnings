33dd810215a0ae56ed5edb799806b429fd20438e
pegalearn1 <pegalearn1@gmail.com>
2026-02-11 18:51:37 +0530

    updated fastapi main

--- FastAPI_AI/main.py
+++ FastAPI_AI/main.py
@@ -5,3 +5,13 @@ app = FastAPI()
 @app.get("/")
 def home():
 	return {"message":"Hello FastAPI"}
+
+
+@app.get("/items/{item_id}/")
+def read_item(item_id: int, q: str = "Just a query"):
+	return {"item_id": item_id, "q": q}
+
+
+@app.get("/search/")
+def search(q: str, limit: int = 10):
+    return {"query": q, "limit": limit}
